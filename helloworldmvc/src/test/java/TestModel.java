666fb9de5a41daf3c479ded17e1d4c6dff37bd92
DAUNAY <cldaunay@gmail.com>
2019-06-24 16:25:09 +0200

    Implementation TestNG / Junit with one test's method

---
+++ helloworldmvc/src/test/java/TestModel.java
@@ -0,0 +1,17 @@
+import helloworldmvc.model.Model;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestModel {
+
+    @Test
+    public void testGetMessage()
+    {
+        Model model=new Model();
+        String m=model.getMessage();
+        Assert.assertNotNull(m);
+        Assert.assertEquals(m, "Hello Darkness my old friend !!!");
+    }
+
+}
+
